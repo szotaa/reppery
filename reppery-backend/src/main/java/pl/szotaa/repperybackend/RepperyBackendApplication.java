4100caed9a36cfc3de3bbc65bc8f587a6a045381
szotaa <kubaszota97@gmail.com>
2018-07-27 15:03:18 +0200

    Initial backend Spring Boot application

---
+++ reppery-backend/src/main/java/pl/szotaa/repperybackend/RepperyBackendApplication.java
@@ -0,0 +1,12 @@
+package pl.szotaa.repperybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepperyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepperyBackendApplication.class, args);
+	}
+}
