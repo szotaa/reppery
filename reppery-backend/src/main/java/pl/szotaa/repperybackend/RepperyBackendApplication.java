2ac2f98ef8c5cd73cf59232249c0d66df74e4497
szotaa <kubaszota97@gmail.com>
2018-07-28 18:10:43 +0200

    Update Javadocs

--- reppery-backend/src/main/java/pl/szotaa/repperybackend/RepperyBackendApplication.java
+++ reppery-backend/src/main/java/pl/szotaa/repperybackend/RepperyBackendApplication.java
@@ -3,6 +3,12 @@ package pl.szotaa.repperybackend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Spring Boot starting class.
+ *
+ * @author szotaa
+ */
+
 @SpringBootApplication
 public class RepperyBackendApplication {
 
