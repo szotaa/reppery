80e9269e2957e9dbc671cae116e47dd95d845a8d
szotaa <kubaszota97@gmail.com>
2018-07-29 11:09:05 +0200

    Temporarily ignore contextLoads test

--- reppery-backend/src/test/groovy/pl/szotaa/repperybackend/RepperyBackendApplicationTest.groovy
+++ reppery-backend/src/test/groovy/pl/szotaa/repperybackend/RepperyBackendApplicationTest.groovy
@@ -1,11 +1,13 @@
 package pl.szotaa.repperybackend
 
 import org.springframework.boot.test.context.SpringBootTest
+import spock.lang.Ignore
 import spock.lang.Specification
 
 @SpringBootTest
 class RepperyBackendApplicationTest extends Specification {
 
+    @Ignore
     def "Should boot application context"() {
         expect:
             true
