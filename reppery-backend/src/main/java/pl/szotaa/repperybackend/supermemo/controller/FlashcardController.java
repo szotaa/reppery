2e51b0766b86ef325780a81e1216a44ba058fd66
szotaa <kubaszota97@gmail.com>
2018-08-01 13:42:19 +0200

    Initial Flashcard CRUD controller

---
+++ reppery-backend/src/main/java/pl/szotaa/repperybackend/supermemo/controller/FlashcardController.java
@@ -0,0 +1,46 @@
+package pl.szotaa.repperybackend.supermemo.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pl.szotaa.repperybackend.supermemo.domain.Flashcard;
+import pl.szotaa.repperybackend.supermemo.exception.FlashcardNotFoundException;
+import pl.szotaa.repperybackend.supermemo.service.FlashcardService;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/flashcard")
+public class FlashcardController {
+
+    private final FlashcardService flashcardService;
+
+    @PostMapping
+    public ResponseEntity<Void> add(@RequestBody Flashcard flashcard){
+        flashcardService.add(flashcard);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Flashcard> get(@PathVariable long id) throws FlashcardNotFoundException {
+        return ResponseEntity.ok(flashcardService.findById(id));
+    }
+
+    @PutMapping
+    public ResponseEntity<Void> update(@RequestBody Flashcard flashcard) throws FlashcardNotFoundException {
+        flashcardService.update(flashcard);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable long id) throws FlashcardNotFoundException {
+        flashcardService.delete(id);
+        return ResponseEntity.ok().build();
+    }
+}
---
+++ reppery-backend/src/test/groovy/pl/szotaa/repperybackend/supermemo/controller/FlashcardControllerTest.groovy
@@ -0,0 +1,67 @@
+package pl.szotaa.repperybackend.supermemo.controller
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.springframework.http.MediaType
+import org.springframework.test.web.servlet.setup.MockMvcBuilders
+import pl.szotaa.repperybackend.supermemo.domain.Flashcard
+import pl.szotaa.repperybackend.supermemo.service.FlashcardService
+import spock.lang.Ignore
+import spock.lang.Specification
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+
+class FlashcardControllerTest extends Specification {
+
+    def flashcardService = Mock(FlashcardService)
+    def flashcardController = new FlashcardController(flashcardService)
+    def writer = new ObjectMapper().writer().withDefaultPrettyPrinter()
+    def mockMvc = MockMvcBuilders.standaloneSetup(flashcardController).build()
+
+    @Ignore
+    def "POST request on /flashcard should execute flashcardService.add" (){
+        given:
+            def flashcard = Flashcard.builder().id(1).build()
+            def json = writer.writeValueAsString(flashcard)
+        when:
+            def response = mockMvc.perform(post("/flashcard")
+                            .contentType(MediaType.APPLICATION_JSON_UTF8)
+                            .content(json))
+        then:
+            1 * flashcardService.add(_)
+            response.andExpect(status().isOk())
+    }
+
+    def "GET request on /flashcard/{id} should execute flashcardService.findById"(){
+        when:
+            def response = mockMvc.perform(get("/flashcard/5"))
+        then:
+            response.andExpect(status().isOk())
+            1 * flashcardService.findById(_ as Long)
+    }
+
+    @Ignore
+    def "PUT request on /flashcard should execute flashcardService.update"(){
+        given:
+            def flashcard = Flashcard.builder().id(1).build()
+            def json = writer.writeValueAsString(flashcard)
+        when:
+            def response = mockMvc.perform(put("/flashcard/5")
+                                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                                .content(json))
+        then:
+            response.andExpect(status().isOk())
+            1 * flashcardService.update(_)
+    }
+
+    def "DELETE request on /flashcard/{id} should execute flashcardService.delete"(){
+        when:
+            def response = mockMvc.perform(delete("/flashcard/5"))
+        then:
+            response.andExpect(status().isOk())
+            1 * flashcardService.delete(_ as Long)
+    }
+}
