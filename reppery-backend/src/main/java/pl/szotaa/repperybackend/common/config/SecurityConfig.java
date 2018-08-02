a5bd87ae6d6782600a99e7130bb051c810d8a443
szotaa <kubaszota97@gmail.com>
2018-08-02 13:09:17 +0200

    Initial security configuration class

---
+++ reppery-backend/src/main/java/pl/szotaa/repperybackend/common/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package pl.szotaa.repperybackend.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Configuration
+    static class SecurityBeans {
+
+        @Bean
+        public PasswordEncoder getPasswordEncoder(){
+            return new BCryptPasswordEncoder(10);
+        }
+    }
+}
