2ff345d5ca567b1c6a62f7ea59881561311e2419
szotaa <kubaszota97@gmail.com>
2018-08-02 16:48:38 +0200

    Initial JwtAuthenticationFilter

---
+++ reppery-backend/src/main/java/pl/szotaa/repperybackend/auth/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,44 @@
+package pl.szotaa.repperybackend.auth.filter;
+
+import java.io.IOException;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import pl.szotaa.repperybackend.auth.JwtTokenUtil;
+import pl.szotaa.repperybackend.user.domain.User;
+import pl.szotaa.repperybackend.user.service.UserService;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtTokenUtil jwtTokenUtil;
+    private final UserService userService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String jwtToken = getJwtTokenFromRequest(request);
+        if(jwtToken != null && !jwtToken.isEmpty()){
+            String username = this.jwtTokenUtil.getUsernameFromJwt(jwtToken);
+            User user = (User) this.userService.loadUserByUsername(username);
+            UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+            authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+            SecurityContextHolder.getContext().setAuthentication(authToken);
+        }
+    }
+
+    private String getJwtTokenFromRequest(HttpServletRequest request) {
+        String bearer = request.getHeader("Authorization");
+        if (bearer != null && !bearer.isEmpty() && bearer.startsWith("Bearer ")) {
+            return bearer.substring(7);
+        }
+        return null;
+    }
+}
