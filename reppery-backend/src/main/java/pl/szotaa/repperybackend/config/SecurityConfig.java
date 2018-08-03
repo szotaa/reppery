8b62e744ef684e46c58c7b2a38483a75587b69e8
szotaa <kubaszota97@gmail.com>
2018-08-03 13:32:01 +0200

    Move config package to parent directory

--- reppery-backend/src/main/java/pl/szotaa/repperybackend/config/SecurityConfig.java
+++ reppery-backend/src/main/java/pl/szotaa/repperybackend/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package pl.szotaa.repperybackend.common.config;
+package pl.szotaa.repperybackend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
