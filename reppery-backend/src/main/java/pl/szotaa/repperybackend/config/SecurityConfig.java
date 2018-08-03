5eca0303597eb8084b871b5683bd7979c5ef8e6e
szotaa <kubaszota97@gmail.com>
2018-08-03 13:37:15 +0200

    Configure Spring Security

--- reppery-backend/src/main/java/pl/szotaa/repperybackend/config/SecurityConfig.java
+++ reppery-backend/src/main/java/pl/szotaa/repperybackend/config/SecurityConfig.java
@@ -1,16 +1,42 @@
 package pl.szotaa.repperybackend.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import pl.szotaa.repperybackend.user.service.UserService;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final UserService userService;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .cors().disable()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .userDetailsService(this.userService)
+                .passwordEncoder(this.passwordEncoder);
+    }
 
     @Bean
     public AuthenticationManager getAuthenticationManager() throws Exception {
