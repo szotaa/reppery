0adfd615545726274f574ed44799d56ae8e410ed
szotaa <kubaszota97@gmail.com>
2018-08-03 09:34:09 +0200

    Add missing bean declarations

--- reppery-backend/src/main/java/pl/szotaa/repperybackend/common/config/SecurityConfig.java
+++ reppery-backend/src/main/java/pl/szotaa/repperybackend/common/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package pl.szotaa.repperybackend.common.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -10,6 +11,11 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public AuthenticationManager getAuthenticationManager() throws Exception {
+        return super.authenticationManager();
+    }
 
     @Configuration
     static class SecurityBeans {
