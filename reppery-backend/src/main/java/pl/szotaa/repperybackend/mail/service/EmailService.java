d7cbcb9a8dc2d50f122eb7efe96d6eb19bb10503
szotaa <kubaszota97@gmail.com>
2018-08-04 12:07:31 +0200

    Initial EmailService

---
+++ reppery-backend/src/main/java/pl/szotaa/repperybackend/mail/service/EmailService.java
@@ -0,0 +1,31 @@
+package pl.szotaa.repperybackend.mail.service;
+
+import com.sendgrid.Mail;
+import com.sendgrid.Method;
+import com.sendgrid.Request;
+import com.sendgrid.SendGrid;
+import java.io.IOException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class EmailService {
+
+    private final SendGrid sendGrid;
+
+    public void sendEmail(Mail mail){
+        Request request = new Request();
+        try {
+            request.setMethod(Method.POST);
+            request.setEndpoint("mail/send");
+            request.setBody(mail.build());
+            sendGrid.api(request);
+            log.info("Attempting to send email");
+        } catch (IOException e){
+            log.error(e.getMessage());
+        }
+    }
+}
