f26922f5e4e46697cce68529dc5d361dfe3d7df6
szotaa <kubaszota97@gmail.com>
2018-08-04 20:04:38 +0200

    Initial HomeComponent

--- reppery-frontend/src/app/app.module.ts
+++ reppery-frontend/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
