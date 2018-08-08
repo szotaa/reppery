337c7ce64892af5af1728dd36ae8b914e9194991
szotaa <kubaszota97@gmail.com>
2018-08-08 08:52:31 +0200

    Enable cross orgin requests for localhost served Angular application

---
+++ reppery-backend/src/main/java/pl/szotaa/repperybackend/config/DevMvcConfig.java
@@ -0,0 +1,23 @@
+package pl.szotaa.repperybackend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@EnableWebMvc
+@Configuration
+@Profile("dev")
+public class DevMvcConfig implements WebMvcConfigurer {
+
+    /**
+     * Enables cross origin request for localhost-served Angular app for development convenience.
+     */
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+    }
+}
