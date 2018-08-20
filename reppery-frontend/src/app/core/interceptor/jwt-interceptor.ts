bda102ea251201e21e4c0022c5219f06258bd943
szotaa <kubaszota97@gmail.com>
2018-08-20 11:19:25 +0200

    Don't attach expired JWT tokens to request header

--- reppery-frontend/src/app/core/interceptor/jwt-interceptor.ts
+++ reppery-frontend/src/app/core/interceptor/jwt-interceptor.ts
@@ -1,12 +1,18 @@
 import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from "../../../../node_modules/@angular/common/http";
 import {Observable} from "rxjs";
 import {Injectable} from "@angular/core";
+import {AuthService} from "../service/auth.service";
 
 @Injectable()
 export class JwtInterceptor implements HttpInterceptor{
+
+  constructor(
+    private auth: AuthService
+  ){}
+
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     const jwtToken = localStorage.getItem('jwtToken');
-    if (jwtToken) {
+    if (this.auth.isAuthenticated()) {
       const cloned = req.clone({
         headers: req.headers.set('Authorization', 'Bearer ' + jwtToken)
       });
