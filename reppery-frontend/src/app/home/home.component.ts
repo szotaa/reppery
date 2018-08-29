c0a2be2cecbc4430772b27a5f9205af0bb13c288
szotaa <kubaszota97@gmail.com>
2018-08-29 13:39:22 +0200

    Fix auth service not accessible in home component

--- reppery-frontend/src/app/home/home.component.ts
+++ reppery-frontend/src/app/home/home.component.ts
@@ -11,7 +11,7 @@ export class HomeComponent implements OnInit {
 
   constructor(
     private title: Title,
-    private auth: AuthService) { }
+    public auth: AuthService) { }
 
   ngOnInit() {
     this.title.setTitle('Reppery');
