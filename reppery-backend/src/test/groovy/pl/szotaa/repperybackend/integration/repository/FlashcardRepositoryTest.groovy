99f4c3cc7bc3cb5807e4a7c5e67ea23b00bd4db4
szotaa <kubaszota97@gmail.com>
2018-08-29 13:21:55 +0200

    Fix @Ignore annotation accidentaly imported from junit intead of spock package

--- reppery-backend/src/test/groovy/pl/szotaa/repperybackend/integration/repository/FlashcardRepositoryTest.groovy
+++ reppery-backend/src/test/groovy/pl/szotaa/repperybackend/integration/repository/FlashcardRepositoryTest.groovy
@@ -1,6 +1,5 @@
 package pl.szotaa.repperybackend.integration.repository
 
-import org.junit.Ignore
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
@@ -9,6 +8,7 @@ import pl.szotaa.repperybackend.supermemo.domain.Deck
 import pl.szotaa.repperybackend.supermemo.domain.Flashcard
 import pl.szotaa.repperybackend.supermemo.repository.FlashcardRepository
 import pl.szotaa.repperybackend.user.domain.User
+import spock.lang.Ignore
 import spock.lang.Specification
 
 import java.time.LocalDate
